313ab531873c3ebe149524af445dbea297b7211c
Owen Griffiths <root@ogriffiths.com>
2023-01-25 01:38:23 +0000

    Telegraphed for upcoming 1.3 switchover

--- yark/cli.py
+++ yark/cli.py
@@ -1,16 +1,14 @@
 """Homegrown cli for managing archives"""
 
-import json
 from pathlib import Path
 from colorama import Style, Fore
 import sys
 import threading
 import webbrowser
-import urllib3
-from importlib.metadata import version
 from .errors import _err_msg, ArchiveNotFoundException
 from .channel import Channel, DownloadConfig
 from .viewer import viewer
+import datetime
 
 HELP = f"yark [options]\n\n  YouTube archiving made simple.\n\nOptions:\n  new [name] [url]         Creates new archive with name and channel url\n  refresh [name] [args?]   Refreshes/downloads archive with optional config\n  view [name?]             Launches offline archive viewer website\n  report [name]            Provides a report on the most interesting changes\n\nExample:\n  $ yark new owez https://www.youtube.com/channel/UCSMdm6bUYIBN0KfS2CVuEPA\n  $ yark refresh owez\n  $ yark view owez"
 """User-facing help message provided from the cli"""
@@ -27,18 +25,6 @@ def _cli():
         print(HELP, file=sys.stderr)
         _err_msg(f"\nError: No arguments provided")
         sys.exit(1)
-
-    # Version announcements before going further
-    try:
-        _pypi_version()
-    except Exception as err:
-        _err_msg(
-            f"Error: Failed to check for new Yark version, info:\n"
-            + Style.NORMAL
-            + str(err)
-            + Style.BRIGHT,
-            True,
-        )
 
     # Help
     if args[0] in ["help", "--help", "-h"]:
@@ -195,34 +181,6 @@ def _cli():
         sys.exit(1)
 
 
-def _pypi_version():
-    """Checks if there's a new version of Yark and tells the user if it's significant"""
-    # Get package data from PyPI
-    http = urllib3.PoolManager()
-    req = http.request("GET", "https://pypi.org/pypi/yark/json")
-    data = json.loads(req.data.decode("utf-8"))
-
-    def decode_version(version: str) -> tuple:
-        """Decodes stringified versioning into a tuple"""
-        return tuple([int(v) for v in version.split(".")[:2]])
-
-    # Generate versions
-    our_major, our_minor = decode_version(version("yark"))
-    their_major, their_minor = decode_version(data["info"]["version"])
-
-    # Compare versions
-    if their_major > our_major:
-        print(
-            Fore.YELLOW
-            + f"There's a major update for Yark ready to download! Run `pip3 install --upgrade yark`"
-            + Fore.RESET
-        )
-    elif their_minor > our_minor:
-        print(
-            f"There's a small update for Yark ready to download! Run `pip3 install --upgrade yark`"
-        )
-
-
 def _err_archive_not_found():
     """Errors out the user if the archive doesn't exist"""
     _err_msg("Archive doesn't exist, please make sure you typed it's name correctly!")
@@ -234,3 +192,28 @@ def _err_no_help():
     print(HELP)
     print("\nThere's no additional help for this command")
     sys.exit(0)
+
+
+def _upgrade_messaging() -> None:
+    """
+    Give users some info on the new Yark 1.3 version because because PyPI releases aren't supported
+
+    This wouldn't happen normally but users might be confused seeing as we're switching distribution methods.
+    """
+    # Major update message for 1.3
+    print(
+        Style.BRIGHT
+        + "Yark 1.3 is out now! Go to https://github.com/Owez/yark to download"
+        + Style.DIM
+        + " (pip is no longer supported)"
+        + Style.NORMAL
+    )
+
+    # Give a warning if it's been over a year since release
+    if datetime.datetime.utcnow().year >= 2024:
+        print(
+            Fore.YELLOW
+            + "You're currently on an outdated version of Yark"
+            + Fore.RESET,
+            file=sys.stderr,
+        )
