0ccd5913a118a089e6957fdd1e5ee7250d10aaa6
Owen Griffiths <root@ogriffiths.com>
2023-03-02 12:05:41 +0000

    Set the slug in load to the filename of path

--- yark-pages/src/routes/(start)/load/local/+page.svelte
+++ yark-pages/src/routes/(start)/load/local/+page.svelte
@@ -71,7 +71,7 @@
 	/**
 	 * Imports archive which already exists in the API
 	 */
-	async function importExistingArchive() {
+	function importExistingArchive() {
 		// Validate name only
 		if (name == undefined || nameCompletelyInvalid) {
 			return;
@@ -81,6 +81,28 @@
 		const importedArchive = { server: LOCAL_SERVER, slug: name };
 		setCurrentArchive(importedArchive);
 	}
+
+	/**
+	 * Gets the last filename (or directory name) on a provided path or returns nothing
+	 * @param path Path to get the filename of
+	 */
+	function getPathFilename(path: string): string | undefined {
+		// Don't do anything with no path
+		if (path == undefined || path == '') {
+			return undefined;
+		}
+
+		// Get the last path on the directory and return if possible
+		let filename = path.split('\\').pop();
+		if (filename == undefined) {
+			return undefined;
+		}
+		filename = filename.split('/').pop();
+		return filename;
+	}
+
+	// Set the name to the filename if it ever changes
+	$: name = getPathFilename(path) ?? '';
 </script>
 
 <div class="centre-h">
@@ -95,8 +117,13 @@
 		<Name bind:name bind:nameCompletelyInvalid placeholder="New name" />
 		<button on:click={async () => importOldArchive()} class="bright">Import</button>
 		<h2 class="card-heading">Use Existing</h2>
-		<Name bind:name bind:nameCompletelyInvalid placeholder="Previously-saved archive name" mini={false} />
-		<button on:click={async () => importExistingArchive()} class="bright">Use</button>
+		<Name
+			bind:name
+			bind:nameCompletelyInvalid
+			placeholder="Previously-saved archive name"
+			mini={false}
+		/>
+		<button on:click={() => importExistingArchive()} class="bright">Use</button>
 		<LoadList count={10} />
 	</StartCard>
 </div>
