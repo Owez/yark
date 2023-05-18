80b920a5e9e6a27bf1b85b986c5e15f0ac5dda89
Owen Griffiths <root@ogriffiths.com>
2023-05-18 11:03:03 +0100

    Removed preloading for now

--- ui/src/lib/state.ts
+++ ui/src/lib/state.ts
@@ -15,7 +15,7 @@ export interface VideosSnapshot {
  * @param seconds Seconds to add; defaults to `30`
  * @returns If it's valid
  */
-export function videosSnapshotValid(snapshot?: VideosSnapshot, seconds?: number): boolean {
+function videosSnapshotValid(snapshot?: VideosSnapshot, seconds?: number): boolean {
     if (snapshot == undefined) {
         return false
     }
@@ -31,51 +31,50 @@ export function videosSnapshotValid(snapshot?: VideosSnapshot, seconds?: number)
  * @param videos Videos list to use as data
  * @returns New snapshot for now
  */
-export function newVideosSnapshot(videos: Video[]): VideosSnapshot {
+function newVideosSnapshot(videos: Video[]): VideosSnapshot {
     return {
         taken: new Date(),
         videos: videos
     }
 }
 
-// NOTE: we manually preload right now in the root +layout.server.ts; this is the alternative
-// /**
-//  * Gets an up-to-date videos list for an archive state
-//  * @param state State of archive
-//  * @param kind Kind of videos list to get (doesn't include {@link ArchiveKind.Meta})
-//  * @param base (Optional) The base URL for the API request
-//  * @returns Up-to-date videos list for {@link kind} selected
-//  */
-// export async function getVideosList(state: ArchiveState, kind: ArchiveKind, base?: URL): Promise<Video[]> {
-//     switch (kind) {
-//         case ArchiveKind.Videos:
-//             if (state.videos != undefined && videosSnapshotValid(state.videos)) {
-//                 return state.videos.videos
-//             }
-//             const newVideos = await getArchiveVideos(state.meta.id, kind, base)
-//             state.videos = newVideosSnapshot(newVideos)
-//             return newVideos
+/**
+ * Gets an up-to-date videos list for an archive state
+ * @param state State of archive
+ * @param kind Kind of videos list to get (doesn't include {@link ArchiveKind.Meta})
+ * @param base (Optional) The base URL for the API request
+ * @returns Up-to-date videos list for {@link kind} selected
+ */
+export async function getVideosList(state: ArchiveState, kind: ArchiveKind, base?: URL): Promise<Video[]> {
+    switch (kind) {
+        case ArchiveKind.Videos:
+            if (state.videos != undefined && videosSnapshotValid(state.videos)) {
+                return state.videos.videos
+            }
+            const newVideos = await getArchiveVideos(state.meta.id, kind, base)
+            state.videos = newVideosSnapshot(newVideos)
+            return newVideos
 
-//         case ArchiveKind.Livestreams:
-//             if (state.videos != undefined && videosSnapshotValid(state.videos)) {
-//                 return state.videos.videos
-//             }
-//             const newLivestreams = await getArchiveVideos(state.meta.id, kind, base)
-//             state.livestreams = newVideosSnapshot(newLivestreams)
-//             return newLivestreams
+        case ArchiveKind.Livestreams:
+            if (state.videos != undefined && videosSnapshotValid(state.videos)) {
+                return state.videos.videos
+            }
+            const newLivestreams = await getArchiveVideos(state.meta.id, kind, base)
+            state.livestreams = newVideosSnapshot(newLivestreams)
+            return newLivestreams
 
-//         case ArchiveKind.Shorts:
-//             if (state.videos != undefined && videosSnapshotValid(state.videos)) {
-//                 return state.videos.videos
-//             }
-//             const newShorts = await getArchiveVideos(state.meta.id, kind, base)
-//             state.shorts = newVideosSnapshot(newShorts)
-//             return newShorts
+        case ArchiveKind.Shorts:
+            if (state.videos != undefined && videosSnapshotValid(state.videos)) {
+                return state.videos.videos
+            }
+            const newShorts = await getArchiveVideos(state.meta.id, kind, base)
+            state.shorts = newVideosSnapshot(newShorts)
+            return newShorts
 
-//         default:
-//             throw new Error("Meta-information archive kind isn't a valid videos list")
-//     }
-// }
+        default:
+            throw new Error("Meta-information archive kind isn't a valid videos list")
+    }
+}
 
 /**
  * Archive state representing a fully saved archive
--- ui/src/routes/+layout.server.ts
+++ ui/src/routes/+layout.server.ts
@@ -13,34 +13,6 @@ export async function load({ cookies, url }): Promise<LayoutServerLoad> {
         archiveState = parsed
     }
 
-    // Get archive kind and preload video lists
-    const archiveKindQuery = url.searchParams.get("kind");
-    let archiveKind = ArchiveKind.Meta;
-    if (archiveKindQuery != null) {
-        switch (archiveKindQuery) {
-            case "videos":
-                archiveKind = ArchiveKind.Videos
-                if (archiveState != null && !videosSnapshotValid(archiveState.videos)) {
-                    archiveState.videos = newVideosSnapshot(await getArchiveVideos(archiveState.meta.id, archiveKind))
-                }
-                break;
-            case "livestreams":
-                archiveKind = ArchiveKind.Livestreams
-                if (archiveState != null && !videosSnapshotValid(archiveState.livestreams)) {
-                    archiveState.livestreams = newVideosSnapshot(await getArchiveVideos(archiveState.meta.id, archiveKind))
-                }
-                break;
-            case "shorts":
-                archiveKind = ArchiveKind.Shorts
-                if (archiveState != null && !videosSnapshotValid(archiveState.shorts)) {
-                    archiveState.shorts = newVideosSnapshot(await getArchiveVideos(archiveState.meta.id, archiveKind))
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
     // Return data
-    return { archiveState, archiveKind }
+    return { archiveState }
 }
