42704632d2fbba58da0b0ab28d962361a86a8830
Owen Griffiths <root@ogriffiths.com>
2023-05-20 11:53:33 +0100

    Started on sidebar

---
+++ ui/src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <div class="sidebar" />
+</nav>
+
+<style lang="scss">
+    $sidebar: 50px;
+    $gap: 10px;
+    $gap-double: $gap * 2;
+
+    nav {
+        height: 100vh;
+        width: calc($sidebar + $gap-double);
+        flex-shrink: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sidebar {
+        height: calc(100vh - $gap-double);
+        width: $sidebar;
+        background-color: black;
+        border-radius: 5px;
+    }
+</style>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar />
