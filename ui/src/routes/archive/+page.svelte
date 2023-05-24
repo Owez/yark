415271dd36963f89631e83e5d667f91e47f9c20c
Owen Griffiths <root@ogriffiths.com>
2023-05-24 15:15:25 +0100

    Dash screen

--- ui/src/routes/archive/+page.svelte
+++ ui/src/routes/archive/+page.svelte
@@ -1,1 +1,75 @@
-<p>Archive meta</p>
+<div class="content">
+    <h1>Owen's Channel</h1>
+    <div class="cards">
+        <a href="/archive/videos" class="card count">
+            <p class="number">10</p>
+            <p>Videos</p>
+        </a>
+        <a href="/archive/livestreams" class="card count">
+            <p class="number">10</p>
+            <p>Livestreams</p>
+        </a>
+        <a href="/archive/shorts" class="card count">
+            <p class="number">10</p>
+            <p>Shorts</p>
+        </a>
+        <div class="card videos">TODO: videos</div>
+        <div class="card videos">TODO: livestreams</div>
+        <div class="card videos">TODO: shorts</div>
+    </div>
+    <div class="card settings"><p>TODO: settings</p></div>
+</div>
+
+<style lang="scss">
+    $gap: 30px;
+
+    .content {
+        margin-left: $gap;
+        margin-right: $gap;
+    }
+
+    h1 {
+        font-size: 2rem;
+        font-weight: 500;
+        margin-top: 2.25rem;
+        margin-bottom: $gap;
+    }
+
+    .cards {
+        width: 100%;
+        display: grid;
+        grid-template-columns: auto auto auto;
+        gap: $gap;
+    }
+
+    .card {
+        border-radius: 10px;
+        border: 1px solid rgba($color: #000000, $alpha: 0.12);
+        min-width: 15rem;
+        width: 100%;
+
+        &.count {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 7rem;
+            text-decoration: none;
+            color: inherit;
+
+            p.number {
+                font-size: 2.5rem;
+                margin-bottom: 0.35rem;
+            }
+        }
+
+        &.videos {
+            min-height: 10rem;
+            height: 50vh;
+        }
+
+        &.settings {
+            margin-top: $gap;
+        }
+    }
+</style>
